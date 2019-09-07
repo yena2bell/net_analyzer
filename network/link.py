a1799f4200b3b8c86f8a4f38c7e0b438e7237495
Jongwan Kim <yena2bell@kaist.ac.kr>
2019-09-07 12:47:29 +0900

    link.py: in Directed_link class, modality attribute and methods changing or showing modality are added

--- network/link.py
+++ network/link.py
@@ -9,6 +9,7 @@ class Directed_link:
     def __init__(self, Node_start, Node_end):
         self.node_start = Node_start
         self.node_end = Node_end
+        self.b_modality = None#activation->True inhibition->False
         
     def __repr__(self):
         return "{}->{}".format(self.node_start, self.node_end)
@@ -18,3 +19,12 @@ class Directed_link:
     
     def show_end_node(self):
         return self.node_end
+    
+    def is_activating_link(self):
+        if self.b_modality == None:
+            raise ValueError(str(self)+" link's modality is not determined")
+        else:
+            return self.b_modality
+    
+    def change_modality(self, b_modality):
+        self.b_modality = b_modality
