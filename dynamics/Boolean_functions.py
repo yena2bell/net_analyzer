11b527ee9ae0b4e3e772df32ed1da8f0d09aead1
Jongwan Kim <yena2bell@kaist.ac.kr>
2019-09-11 14:14:16 +0900

    Boolean_functions.py: parse_minimized_Boolean_logic_equation_tolist -> parse_Boolean_logic_equation_to_list. this function is more generalized. convert_Boolean_logic_equation_to_integer_form_truthtable function is added. it convert string of Boolean logic equation to Boolean truthtable

--- dynamics/Boolean_functions.py
+++ dynamics/Boolean_functions.py
@@ -105,51 +105,32 @@ def get_minimized_Boolean_logic_equation_using_Quine_McCluskey_algorithm_from_tr
     qm = QM(ls_orderd_regulators)
     s_logic_equation = qm.get_function(qm.solve(l_i_states_of_regulators,[])[1])
     if b_return_listform:
-        return parse_minimized_Boolean_logic_equation_to_list(s_logic_equation)
+        return parse_Boolean_logic_equation_to_list(s_logic_equation)
     else:
         return s_logic_equation
 
-def parse_minimized_Boolean_logic_equation_to_list(s_logic_equation):
+def parse_Boolean_logic_equation_to_list(s_logic_equation, 
+                                         s_result='=',
+                                         s_OR="OR", s_AND="AND", s_NOT="NOT", 
+                                         s_plus='+', s_minus='-', s_biggerequal=">=", s_bigger='>',s_smallerequal="<=",s_smaller='<',s_equal='='):
     """this function is for 'get_minimized_Boolean_logic_equation_using_Quine_McCluskey_algorithm_from_truthtable' function.
     parse the string to list form. 
-    assume that node name don't have space, 'NOT', 'AND', 'OR'"""
+    assume that node name don't have space, node name don't contain s_OR, s_AND, s_NOT,s_equal, ')','('...etc
+    s_OR, s_AND, s_NOT,s_equal, ')','(' are not contained to each other."""
     l_stack = []
     i_position = 0
     i_position_start_of_nodename = 0
+    
+    l_operations = [s_result, s_OR, s_AND, s_NOT, s_plus, s_minus, s_biggerequal, 
+                    s_bigger, s_smallerequal, s_smaller, s_equal]
+    l_operations.sort(key=len, reverse=True)#check longer operation first, ("<=" is checked before "<" checked)
+    
     while i_position < len(s_logic_equation):
         if s_logic_equation[i_position] == '(':
             if i_position>i_position_start_of_nodename:
                 l_stack.append(s_logic_equation[i_position_start_of_nodename:i_position])
                 i_position_start_of_nodename = i_position
             l_stack.append('(')
-            i_position += 1
-            i_position_start_of_nodename += 1
-            
-        elif s_logic_equation[i_position:i_position+3] == "NOT":
-            if i_position>i_position_start_of_nodename:
-                l_stack.append(s_logic_equation[i_position_start_of_nodename:i_position])
-                i_position_start_of_nodename = i_position
-            l_stack.append("NOT")
-            i_position += 3
-            i_position_start_of_nodename += 3
-        elif s_logic_equation[i_position:i_position+3] == "AND":
-            if i_position>i_position_start_of_nodename:
-                l_stack.append(s_logic_equation[i_position_start_of_nodename:i_position])
-                i_position_start_of_nodename = i_position
-            l_stack.append("AND")
-            i_position += 3
-            i_position_start_of_nodename += 3
-        elif s_logic_equation[i_position:i_position+2] == "OR":
-            if i_position>i_position_start_of_nodename:
-                l_stack.append(s_logic_equation[i_position_start_of_nodename:i_position])
-                i_position_start_of_nodename = i_position
-            l_stack.append("OR")
-            i_position += 3
-            i_position_start_of_nodename += 3
-        elif s_logic_equation[i_position] == ' ':
-            if i_position>i_position_start_of_nodename:
-                l_stack.append(s_logic_equation[i_position_start_of_nodename:i_position])
-                i_position_start_of_nodename = i_position
             i_position += 1
             i_position_start_of_nodename += 1
         elif s_logic_equation[i_position] == ')':       
@@ -159,11 +140,90 @@ def parse_minimized_Boolean_logic_equation_to_list(s_logic_equation):
             l_stack.append(')')
             i_position += 1
             i_position_start_of_nodename += 1
-        else:
-            i_position += 1
-            if i_position == len(s_logic_equation):
+        elif s_logic_equation[i_position] == ' ':#space
+            if i_position>i_position_start_of_nodename:
                 l_stack.append(s_logic_equation[i_position_start_of_nodename:i_position])
+                i_position_start_of_nodename = i_position
+            i_position += 1
+            i_position_start_of_nodename += 1
+        else:
+            for s_operation in l_operations:
+                if s_logic_equation[i_position:i_position+len(s_operation)] == s_operation:
+                    if i_position>i_position_start_of_nodename:
+                        l_stack.append(s_logic_equation[i_position_start_of_nodename:i_position])
+                        i_position_start_of_nodename = i_position
+                    l_stack.append(s_operation)
+                    i_position += len(s_operation)
+                    i_position_start_of_nodename += len(s_operation)
+                    break
+            else:
+                i_position += 1
+                if i_position == len(s_logic_equation):#at the end of logic equation
+                    l_stack.append(s_logic_equation[i_position_start_of_nodename:i_position])
+        
     return l_stack
+
+
+def convert_Boolean_logic_equation_to_integer_form_truthtable(s_equation, 
+                                                              ts_order_of_inputs=None,
+                                                              s_result='=',
+                                         s_OR="OR", s_AND="AND", s_NOT="NOT", 
+                                         s_plus='+', s_minus='-', s_biggerequal=">=", s_bigger='>',s_smallerequal="<=",s_smaller='<',s_equal='='):
+    """convert Boolean logic equation to integer form truthtable.
+    s_equation can be "output = Boolean logic relations of regulators" or "Boolean logic relation of regulator without "output = " part.
+    if ts_order_of_inputs == None, function returns order of regulator for that interger form truthtable depending on order of logic equation.
+    in that case, regulator name should contain at least one string. if name ahs only number, it will be considered numeric value.
+        i.e. if float(element-name) has no error, it can be considered float vlaue.
+    if ts_order_of_inputs is (s_regulator1, s_regulator2, s_regulator3,,,), function returns interger form truthtable according to that order."""
+    dic_operation_nominal = {s_OR:"or", s_AND:"and", s_NOT:"not", 
+                             s_plus:'+', s_minus:'-', s_biggerequal:">=", 
+                             s_bigger:'>',s_smallerequal:"<=",s_smaller:'<',s_equal:'=='}
+    ls_equation = parse_Boolean_logic_equation_to_list(s_equation, s_OR, s_AND, s_NOT,s_equal)
+    if ls_equation[1] == s_result:
+        ls_equation = ls_equation[2:]
+        
+    l_operations = [s_result, s_OR, s_AND, s_NOT, s_plus, s_minus, s_biggerequal, 
+                    s_bigger, s_smallerequal, s_smaller, s_equal, '(',')']
+    l_variables = list(set(ls_equation).difference(set(l_operations)))
+    for i in range(len(l_variables)-1,-1,-1):
+        try: 
+            float(l_variables[i])
+            l_variables.pop(i)
+        except ValueError:
+            pass
+    
+    if ts_order_of_inputs:
+        if set(l_variables) != set(ts_order_of_inputs):
+            raise ValueError("element extracted from logic equation is not same to entered ordered_of_inputs data\nextracted from equation: "+str(l_variables)+"\nentered order of inputs: "+str(ts_order_of_inputs))
+        l_variables = list(ts_order_of_inputs)
+    #l_variables become the order data of regulators.
+    
+    for i,s_element in enumerate(ls_equation):
+        for s_operation in dic_operation_nominal.keys():
+            if s_element == s_operation:
+                ls_equation[i] = dic_operation_nominal[s_operation]
+                break
+    #s_operation elements in ls_equation is changed to python standard version
+    
+    i_truthtable = 0
+    i_num_of_regulator = len(l_variables)
+    for i_row in range(pow(2,i_num_of_regulator)):
+        dic_state = dict(zip(l_variables,reversed(("{0:0%db}" %i_num_of_regulator).format(i_row))))
+        
+        ls_equation_row = ls_equation.copy()
+        for j, s_element in enumerate(ls_equation_row):
+            if s_element in dic_state.keys():
+                ls_equation_row[j] = dic_state[s_element]
+        
+        if eval(' '.join(ls_equation_row)):
+            i_truthtable += pow(2,i_row)
+    
+    return i_truthtable, l_variables
+    
+    
+    
+    
+    
 
 
 def make_multidim_Boolean_function_array(i_logic, i_numofinputs):#I can't remember why I made it
